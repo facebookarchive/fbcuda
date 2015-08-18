79c826625d7f4df8c231723a17c2adaa7416cde0
Nicolas Vasilache <ntv@fb.com>
2015-08-18 07:11:25 -0700

    FBMM improvements
    Summary:Last batch of improvements using some experience from the low latency MM and applying to the regular MM.
    Perf looks gooood, need to see what is up on Maxwell but basically it seems we can also drop the CGemm pieces.
    This will be done in big red diffs later once we confirm everything looks fine on Maxwell.
    
    The gist of this diff is I now get to 70% of compute efficiency and 65% are floating point operations.
    It used to be around 45-50%
    
    Reviewed By: soumith
    
    Differential Revision: D2352539
    
    fb-gh-sync-id: 993a370dcf22ac1d52a68100ae0a5817a46157c7
    shipit-source-id: 993a370dcf22ac1d52a68100ae0a5817a46157c7

--- MM-inl.cuh
+++ MM-inl.cuh
@@ -196,6 +196,151 @@ __global__ void transposeMMTiledKernelSmall(const DeviceTensor<Complex, 3> A,
 // UpdateGradInput  : xy times i(b, p) <- o(b, f) . f(f, p)
 template <bool ConjugateTransposeA,
           bool ConjugateTransposeB,
+          int FFTSize,
+          int C_J_Unroll,
+          int C_I_Tile,
+          int C_J_Tile,
+          int ReductionUnroll,
+          bool Accumulate>
+__launch_bounds__(32 * 32, 1) // 64 registers on K40
+__global__ void transposeMMTiledKernelUnrolled(const DeviceTensor<Complex, 3> A,
+                                               const DeviceTensor<Complex, 3> B,
+                                               DeviceTensor<Complex, 3> C,
+                                               Complex invNorm) {
+  assert(C_J_Unroll == blockDim.y);
+  assert(A.getSize(2) == C.getSize(2));
+  assert(B.getSize(2) == C.getSize(2));
+  assert(ConjugateTransposeA || A.getSize(0) == C.getSize(0));
+  assert(!ConjugateTransposeA || A.getSize(1) == C.getSize(0));
+  assert(ConjugateTransposeB || B.getSize(1) == C.getSize(1));
+  assert(!ConjugateTransposeB || B.getSize(0) == C.getSize(1));
+  assert(ConjugateTransposeA || ConjugateTransposeB ||
+         A.getSize(1) == B.getSize(0));
+
+  const int numRed =
+    (ConjugateTransposeA) ? A.getSize(0) : A.getSize(1);
+
+  // const int ubi = C.getSize(0);
+  // assert(C.getSize(0) % (C_I_Tile * gridDim.x) == 0);
+
+  // const int ubj = C.getSize(1);
+  // assert(C.getSize(1) % (C_J_Tile * gridDim.y * blockDim.y) == 0);
+
+  const int ubk = numRed;
+  assert(numRed % ReductionUnroll == 0);
+
+  const int numBatches = A.getSize(2);
+  const int ubxy =
+    ceil(numBatches, (int)(gridDim.z * blockDim.x)) *
+    gridDim.z * blockDim.x;
+
+  // for (int i = C_I_Tile * blockIdx.x; i < ubi; i += C_I_Tile * gridDim.x) {
+  //   for (int j = C_J_Tile * (blockIdx.y * blockDim.y + threadIdx.y);
+  //        j < ubj;
+  //        j += C_J_Tile * gridDim.y * blockDim.y) {
+  {
+    {
+      int i = C_I_Tile * blockIdx.x;
+      int j = C_J_Tile * (blockIdx.y * blockDim.y + threadIdx.y);
+
+      // for (int xy = blockDim.x * blockIdx.z + threadIdx.x;
+      //      xy < ubxy;
+      //      xy += gridDim.z * blockDim.x) {
+      {
+        int xy = blockDim.x * blockIdx.z + threadIdx.x;
+
+        Complex a[C_I_Tile];
+        Complex b[C_J_Tile][ReductionUnroll];
+        Complex c[C_I_Tile][C_J_Tile];
+
+        for (int k = 0; k < ubk; k += ReductionUnroll) {
+          // Load B from device to registers with boundary check and static
+          // optimization of those checks.
+          for (int jj = 0; jj < C_J_Tile; ++jj) {
+            for (int kk = 0; kk < ReductionUnroll; ++kk) {
+              b[jj][kk] = (ConjugateTransposeB) ?
+                ldg(&B[j + jj][k + kk][xy]) : // delay conjugate reduces dep
+                ldg(&B[k + kk][j + jj][xy]);
+            }
+          }
+
+          // Use init to hide some latencies
+          if (k == 0) {
+            for (int ii = 0; ii < C_I_Tile; ++ii) {
+              for (int jj = 0; jj < C_J_Tile; ++jj) {
+                c[ii][jj] = (Accumulate) ?
+                  C[i + ii][j + jj][xy] : Complex(0.0f);
+              }
+            }
+          }
+
+
+          // Load A from device to shared with boundary check and static
+          // optimization of those checks.
+          // Distribute loads across blockIdx.y
+          __shared__ Complex
+            as[C_I_Tile][ReductionUnroll][FFTSize];
+
+          assert(C_I_Tile <= blockDim.y);
+
+          // Kill WAW dependence
+          __syncthreads();
+
+          if (threadIdx.y < C_I_Tile) {
+            int ii = threadIdx.y;
+            for (int kk = 0; kk < ReductionUnroll; ++kk) {
+              as[ii][kk][threadIdx.x] = (ConjugateTransposeA) ?
+                ldg(&A[k + kk][i + ii][xy]) :  // delay conjugate reduces dep
+                ldg(&A[i + ii][k + kk][xy]);
+            }
+          }
+
+          // Kill RAW dependence
+          __syncthreads();
+
+          // Perform partial accumulation
+          for (int kk = 0; kk < ReductionUnroll; ++kk) {
+            for (int ii = 0; ii < C_I_Tile; ++ii) {
+              a[ii] = as[ii][kk][threadIdx.x];
+            }
+            for (int jj = 0; jj < C_J_Tile; ++jj) {
+              for (int ii = 0; ii < C_I_Tile; ++ii) {
+                if (ConjugateTransposeA) {
+                  c[ii][jj] = a[ii].conjugate() * b[jj][kk] + c[ii][jj];
+                } else if (ConjugateTransposeB) {
+                  c[ii][jj] = a[ii] * b[jj][kk].conjugate() + c[ii][jj];
+                } else {
+                  c[ii][jj] = a[ii] * b[jj][kk] + c[ii][jj];
+                }
+              }
+            }
+          }
+        }
+
+        for (int ii = 0; ii < C_I_Tile; ++ii) {
+          for (int jj = 0; jj < C_J_Tile; ++jj) {
+            c[ii][jj].re() *= invNorm.re();
+            c[ii][jj].im() *= invNorm.re();
+            *(C[i + ii][j + jj][xy].dataAs<float2>()) = (float2)(c[ii][jj]);
+          }
+        }
+      }
+    }
+  }
+}
+
+
+// By construction, x * y is contiguous.
+// doall xy
+//   doall i, j
+//     red k
+//       C[i][j][x * y] += A[i][k][x * y] * B[k][j][x * y]
+//
+// UpdateOutput     : xy times o(b, f) <- i(b, p) . conj(f(f, p))
+// AccGradParameters: xy times f(f, p) <- conj(o(b, f)) . i(b, p)
+// UpdateGradInput  : xy times i(b, p) <- o(b, f) . f(f, p)
+template <bool ConjugateTransposeA,
+          bool ConjugateTransposeB,
           int C_XY_Placement_ThreadIdx_X,
           int C_J_Unroll,
           int C_I_Tile,
@@ -244,9 +389,9 @@ __global__ void transposeMMTiledKernel(const DeviceTensor<Complex, 3> A,
     numRed : ceil(numRed, ReductionUnroll) * ReductionUnroll;
   assert(!StaticUnrollReduction || numRed % ReductionUnroll == 0);
 
-  const int NumBatches = A.getSize(2);
+  const int numBatches = A.getSize(2);
   const int ubxy =
-    ceil(NumBatches, (int)(gridDim.z * blockDim.x)) *
+    ceil(numBatches, (int)(gridDim.z * blockDim.x)) *
     gridDim.z * blockDim.x;
 
   for (int i = C_I_Tile * blockIdx.x; i < ubi; i += C_I_Tile * gridDim.x) {
@@ -267,7 +412,7 @@ __global__ void transposeMMTiledKernel(const DeviceTensor<Complex, 3> A,
           // Load B from device to registers with boundary check and static
           // optimization of those checks.
           for (int jj = 0; jj < C_J_Tile; ++jj) {
-            if ((StaticUnrollXY || xy < NumBatches) &&
+            if ((StaticUnrollXY || xy < numBatches) &&
                 (StaticUnrollB ||
                  (ConjugateTransposeB && j + jj < B.getSize(0)) ||
                  (!ConjugateTransposeB && j + jj < B.getSize(1)))) {
@@ -293,7 +438,7 @@ __global__ void transposeMMTiledKernel(const DeviceTensor<Complex, 3> A,
           assert(C_I_Tile <= blockDim.y);
           if (threadIdx.y < C_I_Tile) {
             int ii = threadIdx.y;
-            if ((StaticUnrollXY || xy < NumBatches) &&
+            if ((StaticUnrollXY || xy < numBatches) &&
                 (StaticUnrollA ||
                  (ConjugateTransposeA && i + ii < A.getSize(1)) ||
                  (!ConjugateTransposeA && i + ii < A.getSize(0)))) {
@@ -320,7 +465,7 @@ __global__ void transposeMMTiledKernel(const DeviceTensor<Complex, 3> A,
                   (Accumulate &&
                    (StaticUnrollCI || i + ii < C.getSize(0)) &&
                    (StaticUnrollCJ || j + jj < C.getSize(1)) &&
-                   (StaticUnrollXY || xy < NumBatches)) ?
+                   (StaticUnrollXY || xy < numBatches)) ?
                      C[i + ii][j + jj][xy] : Complex(0.0f);
               }
             }
@@ -342,7 +487,7 @@ __global__ void transposeMMTiledKernel(const DeviceTensor<Complex, 3> A,
           }
         }
 
-        if (StaticUnrollXY || xy < NumBatches) {
+        if (StaticUnrollXY || xy < numBatches) {
           for (int ii = 0;
                ii < C_I_Tile && (StaticUnrollCI || i + ii < C.getSize(0));
                ++ii) {
@@ -402,8 +547,6 @@ void transposeMM(DeviceTensor<float, Dim>& A,
 
 #define INSTANTIATE_FBMM_FULLY_UNROLLED(                                \
   C_J_Unroll,                                                           \
-  C_XY_Placement_ThreadIdx_X,                                           \
-  C_XY_Placement_BlockIdx_Z,                                            \
   C_I_Tile,                                                             \
   C_J_Tile,                                                             \
   ReductionUnroll)                                                      \
@@ -416,26 +559,21 @@ void transposeMM(DeviceTensor<float, Dim>& A,
        (!ConjugateTransposeB && (dcB.getSize(1) % C_J_Tile == 0)));     \
     bool StaticUnrollCI = (dcC.getSize(0) % C_I_Tile == 0);             \
     bool StaticUnrollCJ = (dcC.getSize(1) % (C_J_Unroll * C_J_Tile) == 0); \
-    bool StaticUnrollXY =                                               \
-      (dcC.getSize(2) %                                                 \
-       (C_XY_Placement_BlockIdx_Z * C_XY_Placement_ThreadIdx_X) == 0);  \
     const int numRed =                                                  \
       (ConjugateTransposeA) ? dcA.getSize(0) : dcA.getSize(1);          \
     bool StaticUnrollReduction = (numRed % ReductionUnroll == 0);       \
     if (debug) {                                                        \
       LOG(INFO) << StaticUnrollA << " " << StaticUnrollB << " "         \
                 << StaticUnrollCI << " " << StaticUnrollCJ << " "       \
-                << StaticUnrollXY << " " << StaticUnrollReduction;      \
+                << StaticUnrollReduction;                               \
       LOG(INFO) << StaticUnrollA << " " << StaticUnrollB << " "         \
                 << StaticUnrollCI << " " << StaticUnrollCJ << " "       \
-                << StaticUnrollXY << " " << StaticUnrollReduction;      \
+                << StaticUnrollReduction;                               \
     }                                                                   \
     if (StaticUnrollA && StaticUnrollB && StaticUnrollCI &&             \
-        StaticUnrollCJ && StaticUnrollXY && StaticUnrollReduction) {    \
+        StaticUnrollCJ && StaticUnrollReduction) {                      \
       if (debug) {                                                      \
         LOG(INFO) << "Params: " << C_J_Unroll << " " <<                 \
-          C_XY_Placement_ThreadIdx_X << " " <<                          \
-          C_XY_Placement_BlockIdx_Z << " " <<                           \
           C_I_Tile << " " <<                                            \
           C_J_Tile << " " <<                                            \
           ReductionUnroll;                                              \
@@ -444,23 +582,16 @@ void transposeMM(DeviceTensor<float, Dim>& A,
       CHECK_LE(C_I_Tile, C_J_Unroll);                                   \
       dim3 blocks(ceil(dcC.getSize(0), C_I_Tile),                       \
                   ceil(dcC.getSize(1), C_J_Unroll * C_J_Tile),          \
-                  C_XY_Placement_BlockIdx_Z                             \
-                 );                                                     \
-      dim3 threads(C_XY_Placement_ThreadIdx_X, C_J_Unroll);             \
-      detail::transposeMMTiledKernel<ConjugateTransposeA,               \
-                                     ConjugateTransposeB,               \
-                                     C_XY_Placement_ThreadIdx_X,        \
-                                     C_J_Unroll,                        \
-                                     C_I_Tile,                          \
-                                     C_J_Tile,                          \
-                                     ReductionUnroll,                   \
-                                     true,                              \
-                                     true,                              \
-                                     true,                              \
-                                     true,                              \
-                                     true,                              \
-                                     true,                              \
-                                     Accumulate>                        \
+                  FFTSize / 2 + 1);                                     \
+      dim3 threads(FFTSize, C_J_Unroll);                                \
+      detail::transposeMMTiledKernelUnrolled<ConjugateTransposeA,       \
+                                             ConjugateTransposeB,       \
+                                             FFTSize,                   \
+                                             C_J_Unroll,                \
+                                             C_I_Tile,                  \
+                                             C_J_Tile,                  \
+                                             ReductionUnroll,           \
+                                             Accumulate>                \
         <<<blocks, threads, 0, s>>>(dcA, dcB, dcC, Complex(invNorm));   \
       return;                                                           \
     }                                                                   \
@@ -555,20 +686,18 @@ void transposeMM(DeviceTensor<float, Dim>& A,
   }
 
   // INSTANTIATE_FBMM_FULLY_UNROLLED(C_J_Unroll,
-  //                                 C_XY_Placement_ThreadIdx_X,
-  //                                 C_XY_Placement_BlockIdx_Z,
   //                                 C_I_Tile,
   //                                 C_J_Tile,
   //                                 ReductionUnroll)
   // TODO: Add more instantiations to cover use cases properly
   if (dcA.getSize(2) == 3 * 4) {
   } else if (dcA.getSize(2) == 5 * 8) {
-    INSTANTIATE_FBMM_FULLY_UNROLLED(32,  6, 5, /* */ 8, 2, 2);
-    INSTANTIATE_FBMM_FULLY_UNROLLED(16,  8, 5, /* */ 8, 2, 2);
-    INSTANTIATE_FBMM_FULLY_UNROLLED(12,  8, 5, /* */ 8, 2, 2);
-    INSTANTIATE_FBMM_FULLY_UNROLLED(8,  8, 5, /* */ 8, 2, 2);
-
     constexpr int FFTSize = 8;
+    INSTANTIATE_FBMM_FULLY_UNROLLED(32, /* */ 8, 2, 2);
+    INSTANTIATE_FBMM_FULLY_UNROLLED(16, /* */ 8, 2, 2);
+    INSTANTIATE_FBMM_FULLY_UNROLLED(12, /* */ 8, 2, 2);
+    INSTANTIATE_FBMM_FULLY_UNROLLED( 8, /* */ 8, 2, 2);
+
     INSTANTIATE_FBMM_SMALL_FULLY_UNROLLED(4, 4, 4, 4, 4, 4);
     INSTANTIATE_FBMM_SMALL_FULLY_UNROLLED(4, 4, 4, 4, 2, 4);
     INSTANTIATE_FBMM_SMALL_FULLY_UNROLLED(4, 4, 4, 4, 1, 4);
@@ -596,11 +725,11 @@ void transposeMM(DeviceTensor<float, Dim>& A,
     INSTANTIATE_FBMM_SMALL_FULLY_UNROLLED(2, 2, 2, 1, 1, 4);
     INSTANTIATE_FBMM_SMALL_FULLY_UNROLLED(1, 1, 1, 1, 1, 4);
   } else if (dcA.getSize(2) == 9 * 16) {
-    INSTANTIATE_FBMM_FULLY_UNROLLED(16, 12, 6, /* */ 8, 2, 2);
-    INSTANTIATE_FBMM_FULLY_UNROLLED(12, 12, 6, /* */ 8, 2, 2);
-    INSTANTIATE_FBMM_FULLY_UNROLLED(8, 12, 6, /* */ 8, 2, 2);
-
     constexpr int FFTSize = 16;
+    INSTANTIATE_FBMM_FULLY_UNROLLED(16, /* */ 8, 2, 2);
+    INSTANTIATE_FBMM_FULLY_UNROLLED(12, /* */ 8, 2, 2);
+    INSTANTIATE_FBMM_FULLY_UNROLLED( 8, /* */ 8, 2, 2);
+
     INSTANTIATE_FBMM_SMALL_FULLY_UNROLLED(4, 4, 4, 4, 2, 8);
     INSTANTIATE_FBMM_SMALL_FULLY_UNROLLED(4, 4, 4, 4, 1, 8);
     INSTANTIATE_FBMM_SMALL_FULLY_UNROLLED(4, 4, 4, 2, 2, 8);
@@ -625,10 +754,11 @@ void transposeMM(DeviceTensor<float, Dim>& A,
     INSTANTIATE_FBMM_SMALL_FULLY_UNROLLED(2, 2, 2, 1, 1, 8);
     INSTANTIATE_FBMM_SMALL_FULLY_UNROLLED(1, 1, 1, 1, 1, 8);
   } else if (dcA.getSize(2) == 17 * 32) {
-    INSTANTIATE_FBMM_FULLY_UNROLLED(8, 16, 17, /* */ 8, 2, 2);
-    INSTANTIATE_FBMM_FULLY_UNROLLED(4, 16, 17, /* */ 4, 2, 2);
-
     constexpr int FFTSize = 32;
+    INSTANTIATE_FBMM_FULLY_UNROLLED(8, /* */ 8, 2, 2);
+    INSTANTIATE_FBMM_FULLY_UNROLLED(6, /* */ 4, 2, 2);
+    INSTANTIATE_FBMM_FULLY_UNROLLED(4, /* */ 4, 2, 2);
+
     INSTANTIATE_FBMM_SMALL_FULLY_UNROLLED(4, 4, 4, 2, 2, 16);
     INSTANTIATE_FBMM_SMALL_FULLY_UNROLLED(4, 4, 4, 1, 1, 16);
 
